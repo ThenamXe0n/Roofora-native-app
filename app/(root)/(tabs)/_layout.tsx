bfcd0be36eeb403dcc8cf9368dec5eccf0a03050
ThenamXe0n <thenameet0@gmail.com>
2026-05-21 01:07:58 +0530

    feat /tabs implement dynamic tab layout with admin-specific options for iOS and Android

--- app/(root)/(tabs)/_layout.tsx
+++ app/(root)/(tabs)/_layout.tsx
@@ -1,7 +1,12 @@
+import { useUserStore } from "@/store/userStore"
+import { Ionicons } from "@expo/vector-icons"
+import { Tabs } from "expo-router"
 import { Icon, Label, NativeTabs } from "expo-router/unstable-native-tabs"
+import { Platform } from "react-native"
 
 
-const TabLayout = () => {
+const IOSTabs = () => {
+    const isAdmin = useUserStore(state => state.isAdmin)
     return (
         <NativeTabs>
             <NativeTabs.Trigger name="index">
@@ -12,6 +17,10 @@ const TabLayout = () => {
                 <Icon sf="magnifyingglass" />
                 <Label>Search</Label>
             </NativeTabs.Trigger>
+            {isAdmin && <NativeTabs.Trigger name="create">
+                <Icon sf="plus.circle.fill" />
+                <Label>Add Property</Label>
+            </NativeTabs.Trigger>}
             <NativeTabs.Trigger name="save">
                 <Icon sf="heart" />
                 <Label>Save</Label>
@@ -23,5 +32,71 @@ const TabLayout = () => {
         </NativeTabs>
     )
 }
+const AndroidTab = () => {
+    const isAdmin = useUserStore((state) => state.isAdmin);
 
-export default TabLayout
+    return (
+        <Tabs
+            screenOptions={{
+                headerShown: false,
+                tabBarActiveTintColor: "#000",
+            }}
+        >
+            <Tabs.Screen
+                name="index"
+                options={{
+                    title: "Home",
+                    tabBarIcon: ({ color, size }) => (
+                        <Ionicons name="home" size={size} color={color} />
+                    ),
+                }}
+            />
+
+            <Tabs.Screen
+                name="search"
+                options={{
+                    title: "Search",
+                    tabBarIcon: ({ color, size }) => (
+                        <Ionicons name="search" size={size} color={color} />
+                    ),
+                }}
+            />
+
+            {isAdmin && (
+                <Tabs.Screen
+                    name="create"
+                    options={{
+                        title: "Add Property",
+                        tabBarIcon: ({ color, size }) => (
+                            <Ionicons name="add-circle" size={size} color={color} />
+                        ),
+                    }}
+                />
+            )}
+
+            <Tabs.Screen
+                name="save"
+                options={{
+                    title: "Save",
+                    tabBarIcon: ({ color, size }) => (
+                        <Ionicons name="heart" size={size} color={color} />
+                    ),
+                }}
+            />
+
+            <Tabs.Screen
+                name="profile"
+                options={{
+                    title: "Profile",
+                    tabBarIcon: ({ color, size }) => (
+                        <Ionicons name="person" size={size} color={color} />
+                    ),
+                }}
+            />
+        </Tabs>
+    );
+};
+// 
+export default function TabLayout() {
+    return Platform.OS === "ios" ? <IOSTabs /> : <AndroidTab />
+}
