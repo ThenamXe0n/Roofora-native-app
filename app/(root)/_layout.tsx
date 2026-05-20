a4af39bafe2615639398100baa07c743c384ee2b
ThenamXe0n <thenameet0@gmail.com>
2026-05-20 17:36:17 +0530

    feat /user sync and supabase integration added user synchronization with Supabase and Zustand for admin state management

--- app/(root)/_layout.tsx
+++ app/(root)/_layout.tsx
@@ -1,8 +1,11 @@
+import { useUserSync } from "@/hooks/useUserSync";
 import { useAuth } from "@clerk/expo";
 import { Redirect, Slot } from "expo-router";
 
 export default function RootLayout() {
     const { isLoaded, isSignedIn } = useAuth()
+
+    useUserSync();
 
     if (!isLoaded) return null
     if (!isSignedIn) return <Redirect href="/sign-in" />
